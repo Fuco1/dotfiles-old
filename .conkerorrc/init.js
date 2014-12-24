8c977b9e906b24334cc3f4bfc86a89fc62193ba9
Matus Goljer <dota.keys@gmail.com>
2014-12-24 16:46:00 +0100

    [conkeror] Fix buffer revival

--- .conkerorrc/init.js
+++ .conkerorrc/init.js
@@ -86,63 +86,48 @@ interactive("switch-to-other-buffer",
             });
 define_key(default_global_keymap, "C-x C-a", "switch-to-other-buffer");
 
-
-/// revive a killed buffer
+/// revive buffers
 var kill_buffer_original = kill_buffer_original || kill_buffer;
 
-var killed_buffer_urls = [];
-var killed_buffer_histories = [];
+var killed_buffers = [];
 
-//  remember_killed_buffer
 kill_buffer = function (buffer, force) {
-    var hist = buffer.web_navigation.sessionHistory;
-
-    if (buffer.display_uri_string && hist) {
-        killed_buffer_histories.push(hist);
-        killed_buffer_urls.push(buffer.display_uri_string);
+    if (buffer.display_uri_string) {
+        killed_buffers.push({url: buffer.display_uri_string,
+                             title: buffer.title,
+                             history: buffer.web_navigation.sessionHistory});
     }
 
     kill_buffer_original(buffer,force);
 };
 
-interactive("revive-buffer",
-    "Loads url from a previously killed buffer",
-    function restore_killed_buffer (I) {
-        if (killed_buffer_urls.length !== 0) {
-            var url = yield I.minibuffer.read(
-                $prompt = "Restore killed url:",
-                $completer = all_word_completer($completions = killed_buffer_urls),
-                $default_completion = killed_buffer_urls[killed_buffer_urls.length - 1],
-                $auto_complete = "url",
-                $auto_complete_initial = true,
-                $auto_complete_delay = 0,
-                $match_required);
+interactive("restore-killed-buffer-url", "Loads url from a previously killed buffer",
+            function restore_killed_buffer_url (I) {
+                if (killed_buffers.length !== 0) {
+                    var killed_buffer = yield I.minibuffer.read(
+                        $prompt = "Restore killed buffer url:",
+                        $completer = new all_word_completer($completions = killed_buffers,
+                                                            $get_string = function (x) x.url,
+                                                            $get_description = function (x) x.title),
+                        $default_completion = killed_buffers[killed_buffers.length - 1],
+                        $auto_complete = "url",
+                        $auto_complete_initial = true,
+                        $auto_complete_delay = 0,
+                        $require_match = true
+                    );
 
-            var window = I.window;
-            var creator = buffer_creator(content_buffer);
-            var idx = killed_buffer_urls.indexOf(url);
+                    load_url_in_new_buffer(killed_buffer.url);
 
-            // Create the buffer
-            var buf = creator(window, null);
+                    var buf = I.window.buffers.current;
+                    buf.web_navigation.sessionHistory = killed_buffer.history;
+                    var original_index = buf.web_navigation.sessionHistory.index;
+                    buf.web_navigation.gotoIndex(original_index);
 
-            // Recover the history
-            buf.web_navigation.sessionHistory = killed_buffer_histories[idx];
-
-            // This line may seem redundant, but it's necessary.
-            var original_index = buf.web_navigation.sessionHistory.index;
-            buf.web_navigation.gotoIndex(original_index);
-
-            // Focus the new tab
-            window.buffers.current = buf;
-
-            // Remove revived from cemitery
-            killed_buffer_urls.splice(idx, 1);
-            killed_buffer_histories.splice(idx, 1);
-        } else {
-            I.window.minibuffer.message("No killed buffer urls");
-        }
-    });
-define_key(default_global_keymap, "C-T", "revive-buffer");
+                } else {
+                    I.window.minibuffer.message("No killed buffer urls");
+                }
+            });
+define_key(default_global_keymap, "C-T", "restore-killed-buffer-url");
 
 /// remember download file path
 {
