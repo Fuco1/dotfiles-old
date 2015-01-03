ec9a76caedc5c6d387725e5601dd1645a6151dd1
Matus Goljer <dota.keys@gmail.com>
2015-01-03 13:23:22 +0100

    [conkeror] Use the new password management system

--- .conkerorrc/init.js
+++ .conkerorrc/init.js
@@ -2,11 +2,6 @@
 require("session.js");
 session_auto_save_auto_load = true;
 
-session_pref("signon.rememberSignons", true);
-session_pref("signon.expireMasterPassword", false);
-session_pref("signon.SignonFileName", "signons.txt");
-Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager); // init
-
 require("hide-minibuffer.js");
 require("browse-history.js");
 
