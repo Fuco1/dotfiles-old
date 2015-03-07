bb683fa91dada124b23f5997f7efb3b94d0154d2
Matus Goljer <dota.keys@gmail.com>
2015-03-07 13:39:35 +0100

    [xmonad] Add missing dependency

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,6 +14,7 @@ import XMonad.Hooks.UrgencyHook (withUrgencyHook, NoUrgencyHook (..))
 import XMonad.Prompt.RunOrRaise (runOrRaisePrompt)
 import XMonad.Prompt.Window (windowPromptGoto)
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+import XMonad.Util.ExtensibleState as XS
 import XMonad.Util.Run (spawnPipe, runInTerm)
 import qualified XMonad.StackSet as W
 
